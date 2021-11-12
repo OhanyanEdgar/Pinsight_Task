3e5968b63b0b4f6698ebfe05f180b5b56fab4994
Edgar Ohanyan <edgarohanyan1994@gmail.com>
2021-11-13 01:45:53 +0400

    Added Pop Up Dialog for Del User

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,8 +2,10 @@
 // important
 import { useDispatch } from "react-redux"
 import { useNavigate } from 'react-router-dom';
-import { delUser } from "../state/actions/usersActions";
+// Actions
 import { editUser } from "../state/actions/editUserActions";
+
+import DialogPopUp from "./UserDelDialog";
 
 const User = ({ user }) => {
 
@@ -13,23 +15,25 @@ const User = ({ user }) => {
     const handleEditUser = user => {
         dispatch(editUser(user));
         navigate("/update_user");
-
     }
 
     return (
-        <div className="card p-2 mt-2" style={{ width: 25+'vw' }}>
+        <div className="card p-2 mt-2" style={{ 'maxWidth': 400+'px' }}>
             <h5 className="card-title" >Full name: {user.fullName}</h5>
             <p className="card-text" >Username: {user.username}</p>
             <p className="card-text" >Mail: {user.email}</p>
-            <div className="btn-toolbar d-flex justify-content-around">
+            <div className="btn-toolbar d-flex justify-content-evenly">
                 <button 
                     className="btn btn-sm btn-primary"
-                    onClick={() => handleEditUser(user)}
-                >Edit</button>
-                <button 
+                    onClick={() => handleEditUser(user)}>
+                        Edit
+                </button>
+
+                {/* <button 
                     className="btn btn-sm btn-danger"
                     onClick={() => dispatch(delUser(user.id))}
-                >Delete</button>
+                >Delete</button> */}
+                <DialogPopUp user={user} />
             </div>
         </div>
     );
---
+++ src/components/UserDelDialog.js
@@ -0,0 +1,71 @@
+
+// Important
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+// Actions
+import { delUser } from "../state/actions/usersActions";
+// MUI
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+
+
+
+const DialogPopUp = (props) => {
+  const dispatch = useDispatch()
+  const { onClose, open, user } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle className="text-center">
+        You are about to delete <br/> user {user.fullName}.<br/>Are you Sure ?
+      </DialogTitle>
+      <div className="d-flex justify-content-evenly mb-3">
+        <button 
+          className="btn btn-lg btn-danger"
+          onClick={() => dispatch(delUser(user.id))}>
+            Delete
+        </button>
+        <button 
+          className="btn btn-lg btn-success"
+          onClick={handleClose}>
+            Keep
+        </button>
+      </div>
+    </Dialog>
+  )
+
+
+}
+
+
+const UserDelDialog = ({ user }) => {
+
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    return (
+      <div>
+        <button className="btn btn-sm btn-danger" onClick={handleClickOpen}>Delete</button>
+        <DialogPopUp
+          user={ user }
+          open={ open }
+          onClose={ handleClose }
+        />
+      </div>
+    );
+  }
+
+
+export default UserDelDialog;
